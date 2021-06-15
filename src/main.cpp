155d456253c03ccfffe9db8438c7275b9dc6fe29
Mollier Yoann <yoann.mollier@gmail.com>
2021-06-16 00:00:55 +0200

    retrieve the path natives functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,34 @@
 
 #include "frontend.hpp"
 
+#include <Windows.h>
+//#include <libloaderapi.h>
+
 
 
 //todo generate string ID for each text data stored
 //todo stringify and parse json to save data
 int main()
 {
+	//char fileName[MAX_PATH];
+
+	//GetModuleFileNameA(NULL, fileName, MAX_PATH);
+
+	std::filesystem::path current_path = std::filesystem::current_path();
+
 	std::fstream fs;
-	fs.open("trad.json", std::fstream::in | std::fstream::out | std::fstream::app);
-	fs << "petit test pour le json appendising" << std::endl;
+	fs.open("trad.json", std::fstream::in | std::fstream::out | std::fstream::trunc);
+	/*fs << fileName << std::endl;*/
+
+	std::filesystem::current_path(current_path.string() + "\\h");
+	std::cout << "present path : " << std::filesystem::current_path() << std::endl;
+
+
+
+
+	/*std::filesystem::current_path(path);
+	std::cout << "present path : " << std::filesystem::current_path() << std::endl;*/
 	fs.close();
+
+	return 0;
 }
