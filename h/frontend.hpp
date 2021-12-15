5759402e139a1cb0d1d71ecd63ce68fdc7509fbb
Mollier Yoann <yoann.mollier@gmail.com>
2021-12-15 21:57:39 +0100

    Separation into it's own structure
    add show() functions and functionnality

--- h/frontend.hpp
+++ h/frontend.hpp
@@ -2,3 +2,26 @@
 
 #include "backend.hpp"
 
+#include <string>
+#include <SSS/GL.hpp>
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+
+struct GUI_Text {
+public:
+    void show();
+    Text_data save();
+    char bfr[128] = "";
+    Text_data text;
+};
+
+
+struct GUI_Category {
+public:
+    void show();
+    void export_cat();
+    std::string _name;
+    std::vector<GUI_Text> _tradline;
+};
--- src/frontend.cpp
+++ src/frontend.cpp
@@ -1,1 +1,40 @@
 #include "frontend.hpp"
+
+void GUI_Text::show()
+{
+    ImGui::Columns(2);
+    ImGui::Separator();
+    //sprintf_s(buffer, text2.c_str());
+    ImGui::TextWrapped(text.text.c_str());                              ImGui::NextColumn();
+    ImGui::InputText(text.text_ID.c_str(), bfr, IM_ARRAYSIZE(bfr));     ImGui::NextColumn();
+
+    if (!text.comment.empty()) {
+        ImGui::Columns(1);
+        ImGui::Bullet();
+        ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.33f, 0.51f, 0.07f, 1.0f));
+        ImGui::TextWrapped(text.comment.c_str());
+        ImGui::PopStyleColor(1);
+    }
+}
+
+Text_data GUI_Text::save()
+{
+    return Text_data();
+}
+
+void GUI_Category::show()
+{
+    //ImGui::Columns(3, "mycolumns"); // 3-ways, with border
+
+    if (ImGui::TreeNode(_name.c_str())) {
+        for (auto& td : _tradline) {
+            td.show();
+        }
+        
+        ImGui::Separator();
+
+        /*ImGui::InputTextMultiline("##source", buffer, IM_ARRAYSIZE(buffer), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 1.3));*/
+        ImGui::TreePop();
+
+    }
+}
