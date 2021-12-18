013c281e126d43cc6ba1343c158b404934f04c56
Mollier Yoann <yoann.mollier@gmail.com>
2021-12-18 14:42:58 +0100

    Add : Update window dimension
    Resize the ImGui window every time the GL context is resized

--- h/frontend.hpp
+++ h/frontend.hpp
@@ -15,6 +15,7 @@ public:
     Text_data save();
     char bfr[128] = "";
     Text_data text;
+
 };
 
 
@@ -24,4 +25,39 @@ public:
     void export_cat(uint32_t cat_ID, Traduction_data& data);
     std::string _name;
     std::vector<GUI_Text> _tradline;
+};
+
+class TRANSLATOR {
+public :
+    TRANSLATOR();
+    ~TRANSLATOR();
+
+    void show();
+private : 
+    std::map<std::string, std::string> iso_map;
+    std::vector<std::string> _translated;
+    //Keep track of the first and second language iso code
+    std::string _fl;
+    std::string _sl;
+
+    //CONTEXT BEHAVIOR
+    SSS::GL::Window::Shared _window;
+    SSS::GL::Window::Args _args;
+    static void size_callback(GLFWwindow *ptr, int width, int height);
+
+    //GENERATE THE CATEGORY MAP
+    std::map<uint32_t, GUI_Category> CAT;
+
+
+    //File and project management
+    std::string _translation_folder_path = "translation";
+    std::string _project_name = "";
+    void save();
+    void load(std::string path);
+    
+
+    //INTERFACE
+    void menu_bar();
+    void language_selector();
+
 };
--- src/frontend.cpp
+++ src/frontend.cpp
@@ -2,17 +2,22 @@
 
 void GUI_Text::show()
 {
+    
     ImGui::Columns(2);
     ImGui::Separator();
-    //sprintf_s(buffer, text2.c_str());
-    ImGui::TextWrapped(text.text.c_str());                              ImGui::NextColumn();
-    ImGui::InputText(text.text_ID.c_str(), bfr, IM_ARRAYSIZE(bfr));     ImGui::NextColumn();
+    ImGui::TextWrapped(text.text.c_str());                          ImGui::NextColumn();
+    
+    //Create a non read input ID 
+    const std::string input_ID = "##" + text.comment;
+    ImGui::SetNextItemWidth(-1);
+    ImGui::InputText(input_ID.c_str(), bfr, IM_ARRAYSIZE(bfr));     ImGui::NextColumn();
 
     if (!text.comment.empty()) {
         ImGui::Columns(1);
         ImGui::Bullet();
         ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.33f, 0.51f, 0.07f, 1.0f));
-        ImGui::TextWrapped(text.comment.c_str());
+        const std::string comment_ID = "##" + text.comment;
+        ImGui::TextWrapped(comment_ID.c_str());
         ImGui::PopStyleColor(1);
     }
 }
@@ -29,26 +34,261 @@ Text_data GUI_Text::save()
 
 void GUI_Category::show()
 {
-    //ImGui::Columns(3, "mycolumns"); // 3-ways, with border
-
     if (ImGui::TreeNode(_name.c_str())) {
-        for (auto& td : _tradline) {
+        for (GUI_Text td : _tradline) {
             td.show();
         }
         
         ImGui::Separator();
-
-        /*ImGui::InputTextMultiline("##source", buffer, IM_ARRAYSIZE(buffer), ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 1.3));*/
         ImGui::TreePop();
-
     }
 }
 
 void GUI_Category::export_cat(uint32_t cat_ID, Traduction_data& data)
 {
-    for (auto& tl : _tradline) {
+    for (GUI_Text tl : _tradline) {
         Text_data txt = tl.save();
         txt.category = cat_ID;
         data.text_data.emplace_back(txt);
+    }
+}
+
+TRANSLATOR::TRANSLATOR()
+{
+    // FIRST SETUP OPERATION
+    // Fill the languages ISO code map 
+    std::string iso_file = "iso_codes/iso.json";
+    if (check_folder_exists(iso_file)) {
+        std::cout << "le fichier iso existe" << std::endl;
+    }
+    iso_map = retrieve_iso_codes(iso_file);
+
+
+
+    // Check if the folder traduction exist and create/update the file in it
+    std::string folder = "translation";
+    if (!check_folder_exists(folder)) {
+        create_folder(folder);
+    }
+
+
+    ////Go into the traduction folder
+    //std::filesystem::current_path(folder);
+
+    ////Create a new work traduction folder 
+    //std::string workfolder = "group_traduction";
+    //if (!check_folder_exists(workfolder)) {
+    //    create_folder(workfolder);
+    //}
+
+    //std::filesystem::current_path(workfolder);
+
+
+    //  Create a window
+    _args.title = "SSS Translator";
+    _args.w = 1280;
+    _args.h = 720;
+    _window = SSS::GL::Window::create(_args);
+    _window->setVSYNC(true);
+
+}
+
+TRANSLATOR::~TRANSLATOR()
+{
+}
+
+void TRANSLATOR::show()
+{
+    //LOAD FUNCTION
+    Traduction_data mt;
+    mt.parse_traduction_data_from_json("test.json");
+    for (unsigned int i = 0; i < mt.text_data.size(); i++) {
+        /*std::cout << mt.text_data[i].text << std::endl;*/
+    }
+
+    //  CREATE THE GUI CATEGORIES MAP
+    for (auto& m : mt.categories) {
+        GUI_Category cat;
+        cat._name = m.second;
+        CAT.insert(std::make_pair(m.first, cat));
+    }
+
+    //  FILL EACH CATEGORY WITH ITS TEXTS
+    for (auto& t : mt.text_data) {
+        GUI_Text _line;
+        _line.text = t;
+        CAT[t.category]._tradline.emplace_back(_line);
+    }
+
+    //  Setup Dear ImGui window
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::GetIO().IniFilename = nullptr;
+
+    //  Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(_window->getGLFWwindow(), true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Main loop
+    while (!_window->shouldClose()) {
+        // Set GLFW context
+        SSS::GL::Context const context(_window);
+        // Feed inputs to dear imgui, start new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        //FLAGS
+        ImGuiWindowFlags window_flags = 0;
+        window_flags |= ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoTitleBar;
+        window_flags |= ImGuiWindowFlags_MenuBar;
+
+        ImGui::Begin("First language", NULL, window_flags);
+        ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
+        //Update the window dimensions
+        _window.get()->getDimensions(_args.w, _args.h);
+        ImGui::SetWindowSize(ImVec2(_args.w, _args.h), ImGuiCond_Always);
+
+        //Display the menu bar
+        menu_bar();
+
+        //Display the categories
+        for (std::pair<uint32_t, GUI_Category> c : CAT) {
+            c.second.show();
+        }
+
+
+        // Render dear imgui into screen
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        _window->printFrame();
+    }
+
+    //Save function
+    //  SETUP THE TRANSLATED FILE
+    Traduction_data dt;
+    dt.magnitude = mt.magnitude + 1;
+    dt.trad_ID = mt.trad_ID;
+    dt.categories = mt.categories;
+    dt.language = "fra";
+
+    //  EXPORT TRANSLATION INTO JSON FILE
+    for (std::pair<uint32_t, GUI_Category> c : CAT)
+    {
+        c.second.export_cat(c.first, dt);
+    }
+
+    nlohmann::json ej;
+    dt.parse_traduction_data_to_json("trad_fr.json", true);
+
+    // Clean up ImGUI
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
+
+
+void TRANSLATOR::save()
+{
+}
+
+void TRANSLATOR::load(std::string path)
+{
+}
+
+void TRANSLATOR::menu_bar()
+{
+    ImGui::PushItemWidth(ImGui::GetFontSize() * -12);
+    
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("Menu"))
+        {
+            //SAVE CURRENT PROGRESSION
+            if (ImGui::MenuItem("Save"))
+            {
+                save();
+                std::cout << "saved";
+            }
+
+            ImGui::EndMenu();
+        }
+
+
+        language_selector();
+
+
+        ImGui::EndMenuBar();
+    }
+}
+
+void TRANSLATOR::language_selector()
+{
+    ImGui::Text("        Traduction :");
+    int width = 150;
+
+    //static int item_current_idx = 0; // Here we store our selection data as an index.
+    //initialize to the first item of the iso languages list
+    static std::string item_current_idx = iso_map.begin()->first;
+    //reviewed option
+    const char* combo_preview_value = iso_map[item_current_idx].c_str();
+
+
+    ImGui::SetNextItemWidth(width);
+
+    static std::string iterator = "";
+    if (ImGui::BeginCombo("##first_Language", combo_preview_value))
+    {
+
+        //todo performance drop here
+        //Here need only the languages that have already been translated
+        for (auto& m : iso_map)
+        {
+            iterator = m.first;
+
+            const bool is_selected = (item_current_idx == iterator);
+            if (ImGui::Selectable(m.second.c_str(), is_selected))
+                item_current_idx = iterator;
+
+            if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+            }
+
+        }
+
+        ImGui::EndCombo();
+    }
+
+    ImGui::Text(" TO ");
+    ImGui::SetNextItemWidth(width);
+
+    static std::string item_current_idx_sl = iso_map.begin()->first;
+    const char* combo_preview_value_sl = iso_map[item_current_idx_sl].c_str();  // Pass in the preview value visible before opening the combo (it could be anything)
+    ImGui::SetNextItemWidth(width);
+
+
+    static std::string iterator_sl = "";
+
+    if (ImGui::BeginCombo("##second_language", combo_preview_value_sl))
+    {
+        //todo performance drop here 
+        //Here is the need for the cover of the whole iso map to chose a language to translate
+
+        for (auto& m : iso_map)
+        {
+            iterator_sl = m.first;
+
+            const bool is_selected = (item_current_idx_sl == iterator_sl);
+            if (ImGui::Selectable(m.second.c_str(), is_selected))
+                item_current_idx_sl = iterator_sl;
+
+            if (is_selected) {
+                ImGui::SetItemDefaultFocus();
+            }
+        }
+
+        ImGui::EndCombo();
     }
 }
